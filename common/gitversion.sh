58bbbdee023c03f34f71fee97df900f802375db7
max10 <max_10@gmx.de>
2015-03-17 01:29:30 +0100

    update /common/gitversion.sh

--- common/gitversion.sh
+++ common/gitversion.sh
@@ -4,18 +4,20 @@ CURDIR=$1
 
 if [ -d $CURDIR/../../source/libstb-hal-next ]; then
 	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-next && git log | grep "^commit" | wc -l`-next
-elif [ -d $CURDIR/../../source/libstb-hal-github ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-github && git log | grep "^commit" | wc -l`-github
-elif [ -d $CURDIR/../../source/libstb-hal-martii-github ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-martii-github && git log | grep "^commit" | wc -l`-martii-github
+elif [ -d $CURDIR/../../source/libstb-hal-cst-next ]; then
+	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-cst-next && git log | grep "^commit" | wc -l`-github
+elif [ -d $CURDIR/../../source/libstb-hal-new ]; then
+	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-new && git log | grep "^commit" | wc -l`-martii-github
 else
 	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal && git log | grep "^commit" | wc -l`
 fi
 
 if [ -d $CURDIR/../../source/neutrino-mp-next ]; then
 	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-next && git log | grep "^commit" | wc -l`-next
-elif [ -d $CURDIR/../../source/neutrino-mp-github ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-github && git log | grep "^commit" | wc -l`-github
+elif [ -d $CURDIR/../../source/neutrino-mp-cst-next ]; then
+	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-cst-next && git log | grep "^commit" | wc -l`-github
+elif [ -d $CURDIR/../../source/neutrino-mp-cst-next-new ]; then
+	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-cst-next-new && git log | grep "^commit" | wc -l`-github
 elif [ -d $CURDIR/../../source/neutrino-mp-martii-github ]; then
 	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-martii-github && git log | grep "^commit" | wc -l`-martii-github
 else
